aa56b0cceeeb7f2b58f0f3348163ff3ca5117047
Boris De Vloed <code@devloed.com>
2024-09-11 18:24:39 +0200

    added dispatching rules for FE

--- config/dispatcher/dispatcher.ex
+++ config/dispatcher/dispatcher.ex
@@ -1,15 +1,18 @@
 defmodule Dispatcher do
   use Matcher
   define_accept_types [
-    html: [ "text/html", "application/xhtml+html" ],
-    json: [ "application/json", "application/vnd.api+json" ]
+    html: ["text/html", "application/xhtml+html"],
+    json: ["application/json", "application/vnd.api+json"],
+    upload: ["multipart/form-data"],
+    sparql_json: ["application/sparql-results+json"],
+    any: [ "*/*" ],
   ]
 
   @any %{}
   @json %{ accept: %{ json: true } }
   @html %{ accept: %{ html: true } }
 
-  define_layers [ :static, :services, :fall_back, :not_found ]
+  define_layers [ :api, :frontend, :not_found ]
 
   # In order to forward the 'themes' resource to the
   # resource service, use the following forward rule:
@@ -21,9 +24,41 @@ defmodule Dispatcher do
   # Run `docker-compose restart dispatcher` after updating
   # this file.
 
-  match 
+  ###############################################################
+  #   FRONTEND
+  ###############################################################
 
-  match "/*_", %{ layer: :not_found } do
+
+  match "/assets/*path", %{ layer: :api } do
+    Proxy.forward conn, path, "http://frontend/assets/"
+  end
+
+  match "/assets/*path", %{ layer: :api } do
+    Proxy.forward conn, path, "http://frontend/assets/"
+  end
+
+  match "/@appuniversum/*path", %{ layer: :api } do
+    Proxy.forward conn, path, "http://frontend/@appuniversum/"
+  end
+
+  match "/*path", %{ accept: [:html], layer: :api } do
+    Proxy.forward conn, [], "http://frontend/index.html"
+  end
+
+  match "/*_path", %{ layer: :frontend } do
+    Proxy.forward conn, [], "http://frontend/index.html"
+  end
+
+  match "/groups/*path", %{ accept: [:json], layer: :api} do
+    Proxy.forward conn, path, "http://resource/administrative-units/"
+  end
+
+  ###############################################################
+  #   ERROR HANDLING
+  ###############################################################
+
+  match "/*_", %{accept: [:any], layer: :not_found} do
     send_resp( conn, 404, "Route not found.  See config/dispatcher.ex" )
   end
+
 end
